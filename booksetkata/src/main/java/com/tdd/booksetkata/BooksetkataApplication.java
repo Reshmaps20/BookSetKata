d9981bab704117214f3eb5f802cc9cb389af75c3
reshm <reshm@DESKTOP-F10JV1T>
2024-09-18 23:59:16 +0530

    chore: Initial maven build setup

---
+++ booksetkata/src/main/java/com/tdd/booksetkata/BooksetkataApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.booksetkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksetkataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksetkataApplication.class, args);
+	}
+
+}
