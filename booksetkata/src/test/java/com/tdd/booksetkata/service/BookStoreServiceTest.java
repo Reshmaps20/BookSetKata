74b88911e8f007ac138e8f6e895281737804f8bd
reshm <reshm@DESKTOP-F10JV1T>
2024-09-21 17:46:44 +0530

    test : Refactor test case;Added BeforeEach for executing common code

--- booksetkata/src/test/java/com/tdd/booksetkata/service/BookStoreServiceTest.java
+++ booksetkata/src/test/java/com/tdd/booksetkata/service/BookStoreServiceTest.java
@@ -4,15 +4,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import com.tdd.booksetkata.model.Book;
 
 public class BookStoreServiceTest {
 
+	BookStoreService bookStoreService;
+
+	@BeforeEach
+	public void setup() {
+		bookStoreService = new BookStoreService();
+	}
+
 	@Test
 	void testFetchBooks_ShouldFetchAllTheAvailableBooks_ReturnsBooksList() {
 
-		BookStoreService bookStoreService = new BookStoreService();
 		List<Book> books = bookStoreService.fetchBooks();
 		assertEquals(5, books.size());
 	}
@@ -20,16 +27,14 @@ public class BookStoreServiceTest {
 	@Test
 	public void testCalculatePrice_CalculatePriceForSingleBookPurchase_ReturnsPriceOfSingleBook() {
 
-		BookStoreService bookStoreService = new BookStoreService();
 		List<Book> books = Arrays.asList(Book.CLEAN_CODE);
 		double price = bookStoreService.calculatePrice(books);
 		assertEquals(50.0, price);
 	}
 
 	@Test
-	public void testCalculatePrice_TwoDifferentBooks() {
+	public void testCalculatePrice_TwoDifferentBookPurchaseShouldGive5PercentDiscount_RetrunsTotalPrice() {
 
-		BookStoreService bookStoreService = new BookStoreService();
 		List<Book> books = Arrays.asList(Book.CLEAN_CODE, Book.CLEAN_CODER);
 		double price = bookStoreService.calculatePrice(books);
 		assertEquals(95.0, price);
